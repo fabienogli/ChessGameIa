6b57e2719c7bfb46350801fe1389b6b3e8b96a09
fabienogli <pp.btsig@free.fr>
2017-04-06 09:26:51 +0200

    test main

---
+++ Model/Model.h
@@ -0,0 +1,8 @@
+//
+// Created by Fabien on 06/04/2017.
+//
+
+#ifndef CHESSGAMEIA_MODEL_H
+#define CHESSGAMEIA_MODEL_H
+
+#endif //CHESSGAMEIA_MODEL_H
--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 // Created by Fabien on 06/04/2017.
 //
 
+#include <iostream>
+using namespace std;
 
-
+int main()
+{
+    cout<<"salut"<<endl();
+    return 0;
+}
